7e082019dca6eb4d94fa33f4c445d904a3805ce8
苏龙飞 <13690359+slfdwz@user.noreply.gitee.com>
2024-04-29 17:53:01 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,51 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">首页</RouterLink>
-        <RouterLink to="/about">关于</RouterLink>
-        <RouterLink to="/signUp">注册</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <RouterView />
+  <view>
+    <header>
+      <view class="wrapper">
+        <nav>
+          <RouterLink to="/">首页</RouterLink>
+          <RouterLink to="/about">关于</RouterLink>
+          <RouterLink to="/signUp">注册</RouterLink>
+        </nav>
+      </view>
+    </header>
+    <RouterView />
+  </view>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
 
+  nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
 
+    a {
+      &.router-link-exact-active {
+        color: var(--color-text);
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+        &:hover {
+          background-color: transparent;
+        }
+      }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+      display: inline-block;
+      padding: 0 1rem;
+      border-left: 1px solid var(--color-border);
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+      &:first-of-type {
+        border: 0;
+      }
+    }
+  }
 }
 
 @media (min-width: 1024px) {
@@ -54,25 +53,24 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+      margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    nav {
+      text-align: left;
+      margin-left: -1rem;
+      font-size: 1rem;
+      padding: 1rem 0;
+      margin-top: 1rem;
+    }
   }
 }
 </style>
